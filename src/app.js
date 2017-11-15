7f2c5ce41ed972e7ddd50b0227c7b6b7d184d835
star-citizen <Christoph07@gmail.com>
2017-11-14 21:22:10 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+import { getCharacters } from './reducer/characters/actions';
+
+// store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk),
+));
+
+// fetch characters
+store.dispatch(getCharacters());
+
+require('./index.html');
+
+// creat app
+const container = document.querySelector('#app-container');
+
+// render
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , container
+);
+
+// hot module reloading
+if (module.hot) {
+    module.hot.accept('./components/App', () => {
+        ReactDOM.render(
+          <AppContainer>
+            <App />
+          </AppContainer>
+          , container
+      );
+    });
+}
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CharacterList from './CharacterList';
+
+const App = () =>
+  <div className="container">
+    <div className="row">
+      <CharacterList />
+    </div>
+  </div>;
+
+  export default App;
