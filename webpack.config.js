b44f0c946eb8a2c2963e36b0bbe75c7347ca883b
star-citizen <Christoph07@gmail.com>
2017-11-16 16:15:35 -0500

    uglify build

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,9 @@ module.exports = {
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
   ],
 };
